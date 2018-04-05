172a15b71e7fbd8b5a4c77f14f38c35f401d37ea
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-04-05 20:31:23 +0300

    Rewind

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ extern crate futures;
 
 use tokio_timer::Delay;
 use futures::Future;
+use futures::Async;
 
 use std::time::{Duration, Instant};
 
@@ -35,12 +36,26 @@ impl Watchdog {
     }
 }
 
+/// Result returned from a fired Watchdog.
+/// Can be used to rewind watchdog, preserving `Pet` handles pointing to it.
+pub struct Rewind {
+}
+impl Rewind {
+    fn rewind(dur: Duration) -> Watchdog {
+        unimplemented!()
+    }
+}
+
 impl Future for Watchdog {
-    type Item = ();
+    type Item = Rewind;
     type Error = tokio_timer::Error;
     
-    fn poll(&mut self) -> futures::Poll<(), tokio_timer::Error> {
-        self.del.poll()
+    fn poll(&mut self) -> futures::Poll<Rewind, tokio_timer::Error> {
+        match self.del.poll() {
+            Ok(Async::Ready(())) => Ok(Async::Ready(unimplemented!())),
+            Ok(Async::NotReady) => Ok(Async::NotReady),
+            Err(x) => Err(x),
+        }
     }
 }
 
