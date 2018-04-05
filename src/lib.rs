5548759567ce9b395192da14bb4669f0f896e31f
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-04-05 21:22:28 +0300

    A piece of impl

--- src/lib.rs
+++ src/lib.rs
@@ -8,30 +8,51 @@ use tokio_timer::Delay;
 use futures::Future;
 use futures::Async;
 
+use std::sync::Arc;
+use std::sync::Mutex;
+
 use std::time::{Duration, Instant};
 
 
-pub struct Watchdog {
-    del : Delay,
-    dur : Duration,
-}
+// Or Rc<RefCell<>>, selected by cargo features?
+type H = Arc<Mutex<Option<Delay>>>;
 
-/// Reset/restart the watchdog, so it don't activate
-pub struct Pet {
-    // TODO
-}
-impl Pet {
-    /// Reset/restart the watchdog, so it don't activate
-    pub fn pet(&self) {
-        // TODO
-    }
+pub struct Watchdog {
+    del : H,
+    dur : Duration,
 }
 
 impl Watchdog {
     fn new(dur: Duration) -> Self {
+        let d = Delay::new(Instant::now() + dur);
         Watchdog {
-            del: Delay::new(Instant::now() + dur),
+            del: Arc::new(Mutex::new(Some(d))),
             dur: dur,
+        }
+    }
+    fn handle(&self) -> Pet {
+        self.into()
+    }
+}
+
+/// Reset/restart the watchdog, so it don't activate
+pub struct Pet {
+    del : H,
+}
+impl Pet {
+    /// Reset/restart the watchdog, so it don't activate
+    pub fn pet(&self) {
+        let mut g = self.del.lock().unwrap();
+        if let Some(ref mut x) = *g {
+            x.reset(Instant::now() /* + ?  XXX */);
+        }
+    }
+}
+
+impl<'a> From<&'a Watchdog> for Pet {
+    fn from(w : &'a Watchdog) -> Pet {
+        Pet {
+            del: w.del.clone(),
         }
     }
 }
@@ -51,8 +72,10 @@ impl Future for Watchdog {
     type Error = tokio_timer::Error;
     
     fn poll(&mut self) -> futures::Poll<Rewind, tokio_timer::Error> {
-        match self.del.poll() {
-            Ok(Async::Ready(())) => Ok(Async::Ready(unimplemented!())),
+        let mut g = self.del.lock().unwrap();
+        match g.poll() {
+            Ok(Async::Ready(Some(()))) => Ok(Async::Ready(unimplemented!())),
+            Ok(Async::Ready(None)) => Ok(Async::Ready(unimplemented!())),
             Ok(Async::NotReady) => Ok(Async::NotReady),
             Err(x) => Err(x),
         }
